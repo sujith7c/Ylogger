8e798997a25b0a825f9d373dffba30773221cff4
Sujith Suresan <Sujith Suresan sujith7c@gmail.com>
2019-01-26 20:00:56 +0400

    Added handle bar module, created partial folder, included partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.engine(
+  'hbs', hbs.express4({
+    defaultLayout: __dirname + '/views/layouts/default.hbs',
+    partialsDir: __dirname + '/views/partials',
+    layoutsDir: __dirname + '/views/layouts'
+  })
+);
 
 app.use(logger('dev'));
 app.use(express.json());
